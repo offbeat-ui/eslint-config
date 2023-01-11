536823ebeb845e6560c0bce2ca207a166005a3b4
gavin <gavinwjw@163.com>
2023-01-11 17:27:08 +0800

    chore: update name

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require('eslint-define-config')
-const basic = require('@sxzz/eslint-config-basic')
+const basic = require('@wjw/eslint-config-basic')
 
 module.exports = defineConfig({
   extends: [
-    '@sxzz/eslint-config-basic',
+    '@wjw/eslint-config-basic',
     'plugin:@typescript-eslint/recommended',
   ],
   ignorePatterns: ['auto-import.d.ts', 'components.d.ts'],
